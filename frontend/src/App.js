7702b702a889d6f200ad704beb99e567fc79eaa5
stefan <sawyer280792@gmail.com>
2021-01-19 16:17:08 +0100

    fix git part-1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 
 import NewPlace from './places/pages/NewPlace';
 import User from './user/pages/Users';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
     <Router>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <User />
