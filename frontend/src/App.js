6c58c996891198b65278412dac150808f163482d
ItachiUchiha02 <sawyer280792@gmail.com>
2021-01-19 15:32:40 +0100

    fix git part-3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
+import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
-import User from './user/pages/Users';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <User />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
-}
+};
 
 export default App;
