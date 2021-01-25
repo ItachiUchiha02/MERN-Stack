dc86dd8d0ec45a0314b12ccbee47a6f244be1160
stefan <sawyer280792@gmail.com>
2021-01-25 18:40:57 +0100

    ADD PLACES

---
+++ frontend/src/places/pages/UserPlaces.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Emipre State Building',
+        description: 'Onde of the most famous sky scrapers in the world!',
+        imageUrl: 'https://cdn.vox-cdn.com/thumbor/JrYpHTzbE9qSu85id9CSkQhs7dM=/0x0:2000x1333/1200x800/filters:focal(840x507:1160x827)/cdn.vox-cdn.com/uploads/chorus_image/image/65018226/151006_19_00_22_5DSR9489.0.jpg',
+        address: '20 W 34h St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Emipre State Building',
+        description: 'Onde of the most famous sky scrapers in the world!',
+        imageUrl: 'https://cdn.vox-cdn.com/thumbor/JrYpHTzbE9qSu85id9CSkQhs7dM=/0x0:2000x1333/1200x800/filters:focal(840x507:1160x827)/cdn.vox-cdn.com/uploads/chorus_image/image/65018226/151006_19_00_22_5DSR9489.0.jpg',
+        address: '20 W 34h St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u2'
+    }
+]
+
+const UserPlaces = () => {
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+    return <PlaceList items={loadedPlaces} />
+}
+
+export default UserPlaces;
