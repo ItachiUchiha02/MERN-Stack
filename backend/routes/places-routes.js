0145817bdd301f61246dc5c3c2a5a96cab82d462
stefan <sawyer280792@gmail.com>
2021-02-05 12:49:34 +0100

    backend app start

---
+++ backend/routes/places-routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        location: {
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: '20 W 34th St, New York, NT 10001',
+        creator: 'u1'
+    }
+]
+
+router.get('/:pid', (req, res, next) => {
+    const placeId = req.params.pid; // {pit: 'p1'}
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId;
+    })
+    console.log('GET Request in places');
+    res.json({place});
+});
+
+module.exports = router;
