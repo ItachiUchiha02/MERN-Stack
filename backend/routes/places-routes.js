cfad534a273f93f7e9a4787d7f78106893a04205
stefan <sawyer280792@gmail.com>
2021-02-08 17:06:47 +0100

    node js get post patch

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,28 +1,17 @@
 const express = require('express');
 
+const placesControllers = require('../controllers/places-controllers')
+
 const router = express.Router();
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        location: {
-            lat: 40.7484474,
-            lng: -73.9871516
-        },
-        address: '20 W 34th St, New York, NT 10001',
-        creator: 'u1'
-    }
-]
+router.get('/:pid', placesControllers.getPlaceById);
 
-router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid; // {pit: 'p1'}
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeId;
-    })
-    console.log('GET Request in places');
-    res.json({place});
-});
+router.get('/user/:uid', placesControllers.getPlaceByUserId);
+
+router.post('/', placesControllers.createPlace);
+
+router.patch('/:pid', placesControllers.updatePlace);
+
+router.delete('/:pid', placesControllers.deletePlace);
 
 module.exports = router;
