6804a51fee02188b03077a66ffa52933ed927759
ItachiUchiha02 <sawyer280792@gmail.com>
2021-02-19 19:54:41 +0100

    fix 2 sranja

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -96,7 +96,7 @@ const createPlace = async (req, res, next) => {
     res.status(201).json({place: createdPlace});
 };
 
-const updatePlace = aync (req, res, next) => {
+const updatePlace = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         throw new HttpError('Invalid inputs passed, please check your data', 422);
